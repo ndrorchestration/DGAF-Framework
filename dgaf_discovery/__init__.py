d916af7cca4f5b5fab19ef10acccc15fb930d3e9
Andrew // Ndr "Ender" Hensel <246370637+ndrorchestration@users.noreply.github.com>
2026-09-11 21:19:15 -0400

    style(discovery): apply Black formatting

--- dgaf_discovery/__init__.py
+++ dgaf_discovery/__init__.py
@@ -1,3 +1,5 @@
 """Non-authorizing DGAF discovery harness."""
+
 from .harness import DiscoveryEnvelope, validate_discovery_envelope
+
 __all__ = ["DiscoveryEnvelope", "validate_discovery_envelope"]
